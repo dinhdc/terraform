87b91127cff2014160882c821baebf206ee9fa29
dinh.dc <congdinhpro2k@gmail.com>
2024-02-12 16:38:01 +0700

    update

---
+++ 07-AWS-EC2Instance-and-SecurityGroups/lab/vpc.module.tf
@@ -0,0 +1,43 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${local.name}-${var.vpc_name}"
+  cidr = "10.0.0.0/16"
+
+  azs             = var.vpc_availability_zones
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
+
+  # Database Subnets
+  create_database_subnet_group       = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  database_subnets                   = var.vpc_database_subnets
+
+  #create_database_nat_gateway_route = true
+  #create_database_internet_gateway_route = true
+
+  # NAT Gateways - Outbound Communication
+  enable_nat_gateway = var.vpc_enable_nat_gateway
+  single_nat_gateway = var.vpc_single_nat_gateway
+
+
+  # VPC DNS Parameters
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    Type = "public-subnets"
+  }
+
+  private_subnet_tags = {
+    Type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    Type = "database-subnets"
+  }
+
+  tags = local.common_tags
+
+  vpc_tags = local.common_tags
+}
